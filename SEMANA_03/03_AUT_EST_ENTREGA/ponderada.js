960611cdabe65b7745f263d6579b039a81068abc
Enya-Oliveira <enya.arruda@sou.inteli.edu.br>
2022-10-28 17:33:48 -0300

    ponderada3

---
+++ SEMANA_03/03_AUT_EST_ENTREGA/ponderada.js
@@ -0,0 +1,83 @@
+const express = require('express'); 
+    const app = express();
+
+    const hostname = '127.0.0.1';
+    const port = 3000;
+    const sqlite3 = require('sqlite3').verbose();
+    const DBPATH = 'curriculo.db'; //use o nome que você achar melhor para o banco de dados
+
+    app.use(express.json());
+    app.get('/perfil', (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Access-Control-Allow-Origin', '*'); 
+        var db = new sqlite3.Database(DBPATH); // Abre o banco
+        var sql = 'SELECT nome, idade, cargo FROM perfil ORDER BY cargo DESC';
+        db.all(sql, [],  (err, rows ) => {
+            if (err) {
+                throw err;
+            }
+            res.json(rows);
+        });
+        db.close(); // Fecha o banco
+    });
+    
+    app.use(express.json());
+    app.get('/realizacoes', (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Access-Control-Allow-Origin', '*'); 
+        var db = new sqlite3.Database(DBPATH); // Abre o banco
+        var sql = 'SELECT ano FROM perfil ORDER BY ano DESC';
+        db.all(sql, [],  (err, rows ) => {
+            if (err) {
+                throw err;
+            }
+            res.json(rows);
+        });
+        db.close(); // Fecha o banco
+
+    });
+    app.use(express.json());
+    app.get('/experiencia', (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Access-Control-Allow-Origin', '*'); 
+        var db = new sqlite3.Database(DBPATH); // Abre o banco
+        var sql = 'SELECT nomedaempre, comeco, termino, diplomaobtido FROM experiencia ORDER BY diplomaobtido DESC';
+        db.all(sql, [],  (err, rows ) => {
+            if (err) {
+                throw err;
+            }
+            res.json(rows);
+        });
+        db.close(); // Fecha o banco
+    });
+
+    app.use(express.json());
+    app.get('/sobremim', (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Access-Control-Allow-Origin', '*'); 
+        var db = new sqlite3.Database(DBPATH); // Abre o banco
+        var sql = 'SELECT endereço, email, telefone, descricao, habilidades, personalidade FROM perfil ORDER BY ano DESC';
+        db.all(sql, [],  (err, rows ) => {
+            if (err) {
+                throw err;
+            }
+            res.json(rows);
+        });
+        db.close(); // Fecha o banco
+    });
+
+    app.use(express.json());
+    app.get('/formacao', (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Access-Control-Allow-Origin', '*'); 
+        var db = new sqlite3.Database(DBPATH); // Abre o banco
+        var sql = 'SELECT graduacao, instituicao, comeco, termino diplomaobtido FROM formacao ORDER BY termino DESC';
+        db.all(sql, [],  (err, rows ) => {
+            if (err) {
+                throw err;
+            }
+            res.json(rows);
+        });
+        db.close(); // Fecha o banco
+    });
+    
