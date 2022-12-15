8ca0d8f0d706c771296d1d6b8b1d7786f6b31201
Enya-Oliveira <enya.arruda@sou.inteli.edu.br>
2022-12-15 03:12:54 -0300

    aeeeeee-mais uma vez

--- SEMANA_09/03_AUT_EST_ENTREGA/entrega-semana9/script.js
+++ SEMANA_09/03_AUT_EST_ENTREGA/entrega-semana9/script.js
@@ -1,35 +1,42 @@
-function VpVf() {
+function VpVf(valor) {
+
     var resultado = document.getElementById('resultado');
     var taxa = Number(document.getElementById('taxa').value) / 100;
     var tempo = Number(document.getElementById('tempo').value);
 
     if (valor == 'VP') {
-        let valorFuturo = Number(document.getElementById('ValorF').value);
+        let valorFuturo = Number(document.getElementById('VF').value);
 
-        let valorPresente = ((valorFuturo) / ((1 + taxa) ** (periodo))).toFixed(2);
+        let valorPresente = ((valorFuturo) / ((1 + taxa) ** (tempo))).toFixed(2);
 
-        resultado.innerHTML = `O valor aplicado quando se retira R$${valorFuturo} depois de ${periodo} meses a uma taxa mensal de ${taxa * 100}% é de R$${valorPresente}.`;
+        resultado.innerHTML = `O valor aplicado quando se retira R$${valorFuturo} depois de ${tempo} meses a uma taxa mensal de ${taxa * 100}% é de R$${valorPresente}.`;
     }
     if (valor == 'VF') {
-        let valorPresente = Number(document.getElementById('ValorP').value);
+        let valorPresente = Number(document.getElementById('VP').value);
 
-        let valorFuturo = ((valorPresente) * ((1 + taxa) ** (periodo))).toFixed(2);
+        let valorFuturo = ((valorPresente) * ((1 + taxa) ** (tempo))).toFixed(2);
 
-        ///resultado.innerHTML = `O valor futuro quando se aplica R$${valorPresente} por ${periodo} meses a uma taxa mensal de ${taxa * 100}% é de R$${valorFuturo}.`;
-        document.getElementById('resultado').innerHTML+= `O valor futuro è R$${valorPresente} por ${periodo} meses a uma taxa mensal de ${taxa * 100}% é de R$${valorFuturo}.`;
-}
+        resultado.innerHTML = `O valor futuro quando se aplica R$${valorPresente} por ${tempo} meses a uma taxa mensal de ${taxa * 100}% é de R$${valorFuturo}.`;
+        // document.getElementById('resultado').innerHTML+= `O valor futuro è R$${valorPresente} por ${tempo} meses a uma taxa mensal de ${taxa * 100}% é de R$${valorFuturo}.`;
+};
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+function juros2() {
+    console.log('dd')
+}
+
 function juros() {
+    console.log('aa')
     const capital = $('.form2 #capital').val();
+    console.log(capital)
     const tempo = $('.form2 #tempo').val();
     const taxaJ = $('.form2 #taxaJ').val();
 
     const divResultado = $(".form2 .resultado");
     
     divResultado.html(capital * (1 + ((taxaJ / 100) * tempo)).toFixed(2))
-}
+};
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
